af2ab7ab0da67f6f6d6bd07dd212adcb19ce41d9
sageandpine <jeffgrindley702@gmail.com>
2022-11-22 21:36:29 -0800

    FWD function skips and updates the now playing text. When end of playlist reached, wraps back to start.  Added a bit of color since it's so boring at the moment :)

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
-
-# from mutagen.easyid3 import EasyID3
-# from tkinter.messagebox import showinfo
 import os
 import pygame
 
-# from pydub import AudioSegment
-# from pydub.playback import play
+# from mutagen.easyid3 import EasyID3
+# from tkinter.messagebox import showinfo
 
 # Initialize sound module
 class Triad:
@@ -23,6 +20,10 @@ class Triad:
         # Default attributes
         self.is_playing = is_playing
         self.play_label = "Play"
+        self.current_song = 0
+        self.current_path_dir = ""
+        self.next = ""
+        self.now_song = ""
 
         # Tk Window for application
         root = Tk()
@@ -33,8 +34,6 @@ class Triad:
         # Tk Frame that fits inside window
         mainframe = ttk.Frame(root, padding="3 3 12 12")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-
-        # self.pl_var = tk.Variable(value=self.play_label)
 
         # Buttons for Basic Functions
         self.play_button = ttk.Button(
@@ -54,17 +53,24 @@ class Triad:
         ).grid(column=5, row=3, sticky=W)
         self.open_button = ttk.Button(
             mainframe, text="Open", command=self.open_it
-        ).grid(column=5, row=3, sticky=W)
+        ).grid(column=6, row=3, sticky=W)
 
         # New Frame for listing files when directory chosen
-        self.now_playing_list = ["a song", "another one"]
+        self.now_playing_list = []
+        self.track_index = len(self.now_playing_list)
         self.var = tk.Variable(value=self.now_playing_list)
+        self.now_var = tk.Variable(value=self.now_song)
+        self.now_playing = ttk.Label(
+            mainframe, textvariable=self.now_var, justify=CENTER, padding=5
+        ).grid(column=1, row=5)
 
         # Frame that lists contents of chosen directory
         fileframe = ttk.Frame(root, padding="3 3 12 12")
-        fileframe.grid(columnspan=4, row=1, sticky=W)
-        self.file_window = tk.Listbox(fileframe, listvariable=self.var, height=10)
-        self.file_window.grid(column=1, row=0)
+        fileframe.grid(columnspan=1, row=4, sticky=W)
+        self.file_window = tk.Listbox(
+            fileframe, listvariable=self.var, height=10, width=55, bg="#9F73AB"
+        )
+        self.file_window.grid(columnspan=4, row=2)
 
         # Main Loop Launches the GUI and keeps it running until the program terminates
         root.mainloop()
@@ -74,59 +80,85 @@ class Triad:
         # print(f"load it took on this file: {file}")
         pygame.mixer.music.load(file)
 
-    def change_label(self):
-        """*NOT WORKING ATM -Change label on play button to pause when playing"""
-        pass
-        # Might need to REfresh window to see change
+    def change_label(self, new):
+        """Change text label to reflect song playing."""
+        self.now_var.set(new)
 
-    # if self.is_playing:
-    #     self.play_label = "Pause"
-    # else:
-    #    self.play_label = "Play"
+    def queue_next(self):
+        """Queues next track for playback. If user is at end of playlist, loads 1st track to start over."""
+        if (self.current_song + 1) < len(self.now_playing_list):
+            pygame.mixer.music.queue(
+                self.current_path_dir
+                + "/"
+                + self.now_playing_list[(self.current_song + 1)]
+            )
+        else:
+            pygame.mixer.music.queue(
+                self.current_path_dir + "/" + self.now_playing_list[0]
+            )
 
     def play_it(self):
-        """Play file loaded for playback"""
+        """Play file loaded for playback."""
         pygame.mixer.music.play()
         self.is_playing = True
+        self.queue_next()
 
     def pause_it(self):
-        """Pause File currently playing"""
+        """Pause File currently playing."""
         pygame.mixer.music.pause()
         self.is_playing = False
 
     def unpause_it(self):
-        """UnPause File That is paused. Combine with Pause via With loop"""
+        """UnPause File That is paused. Combine with Pause via With loop."""
         pygame.mixer.music.unpause()
         self.is_playing = True
 
     def fwd_it(self):
-        """FWD should load a new track and play it"""
-        pass
+        """FWD should load a new track and play it. When we come to the end of the playlist, automatically re-starts at track 1."""
+        if (self.current_song + 1) < len(self.now_playing_list):
+            self.current_song = self.current_song + 1
+            self.load_it(
+                self.current_path_dir + "/" + self.now_playing_list[self.current_song]
+            )
+            self.play_it()
+            self.now_song = self.now_playing_list[self.current_song]
+            self.change_label(self.now_song)
+
+        else:
+            self.load_it(self.current_path_dir + "/" + self.now_playing_list[0])
+            self.current_song = 0
+            self.play_it()
+            self.now_song = self.now_playing_list[self.current_song]
+            self.change_label(self.now_song)
 
     def rwd_it(self):
-        """Go back to the begining of currently loaded song"""
+        """Go back to the begining of currently loaded song, if button pressed less than 2 seconds after start, go back to previous track"""
+        # Needs work. tried several ways of tracking time that did not work. Will come back and re-work
         pygame.mixer.music.rewind()
 
     def open_it(self):
-        """Launch dialog box to choose a file or folder to pick from. Opens file/directory chosen and plays"""
+        """Launch dialog box to choose a file or folder to pick music from. Opens file/directory chosen and starts to play."""
         self.now_playing_list.clear()
-        path = str(fd.askdirectory())
-        file_list = os.listdir(path)
+        self.current_path_dir = str(fd.askdirectory())
+        file_list = os.listdir(self.current_path_dir)
         for file in file_list:
-            # print(file)
             if ".mp3" in file:
                 self.now_playing_list.append(file)
         self.now_playing_list.sort()
         songs = self.now_playing_list
         self.update_now_playing(songs)
-        self.load_it(path + "/" + self.now_playing_list[0])
+        self.load_it(
+            self.current_path_dir + "/" + self.now_playing_list[self.current_song]
+        )
         self.play_it()
+        self.now_song = self.now_playing_list[self.current_song]
+        self.change_label(self.now_song)
 
     def update_now_playing(self, songs):
         """Updates the file_window widget."""
         self.file_window.delete(0, END)
         for file in songs:
-            self.file_window.insert(0, file)
+            self.file_window.insert(END, file)
 
 
 T = Triad()
